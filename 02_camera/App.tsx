a8b873876ea39c306512bdce1f7914234f21d7af
Vernon de Goede <vernon@mollie.com>
2020-02-20 18:33:09 +0100

    Initial commit

---
+++ 02_camera/App.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Camera } from "expo-camera";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera style={{ height: 600, width: "100%" }} />
+    </View>
+  );
+}
